30c2d422a93de1a21e73a095d928762e04112185
Johan Malm <jgm323@gmail.com>
2024-12-28 22:31:36 +0000

    plot.py: do not predefine contributors
    ...just add all and then plot the top 15
    
    Helped-by: @Consolatis

--- plot.py
+++ plot.py
@@ -6,35 +6,29 @@ import matplotlib.pyplot as plt
 
 class Graph():
     def __init__(self):
-        self.refs = []
-        self.blame_lines = {
+        """
+        blame_lines will become something like this for labwc:
+        {
             'Johan Malm': [],
-            'ARDiDo': [],
-            'bi4k8': [],
-            'Joshua Ashton': [],
             'Consolatis': [],
-            'John Lindgren': [],
-            'Consus': [],
-            'Ph42oN': [],
-            'Tomi Ollila': [],
-            'tokyo4j': [],
-            'Hiroaki Yamamoto': [],
-            'Andrew J. Hesford': [],
-            'Jens Peters': [],
-            'Simon Long': [],
-            'Christopher Snowhill': [],
-            'David Turner': [],
-            'Tobias Bengfort': [],
-            'droc12345': [],
-            'Orfeas': [],
         }
+        """
+        self.refs = []
+        self.blame_lines = dict()
 
     def add_ref(self, ref):
         self.refs.append(ref)
 
     def add_blame_lines(self, who, nr_lines):
-        if who in self.blame_lines:
-            self.blame_lines[who].append(int(nr_lines))
+        if who not in self.blame_lines:
+            if self.blame_lines:
+                # When adding new users we need to pad out with zeros for all the refs
+                # for which they had not yet contributed
+                size = max(len(v) for v in self.blame_lines.values()) - 1
+                self.blame_lines[who] = [0] * size
+            else:
+                self.blame_lines[who] = list()
+        self.blame_lines[who].append(int(nr_lines))
 
     def add_zeros_for_dormant_contributors(self):
         """
@@ -46,6 +40,8 @@ class Graph():
                 i.append(0)
 
     def plot(self):
+        # -1 to reverse; 15 to show top 15 entries
+        self.blame_lines = dict((k, v) for _, k, v in sorted((sum(v), k, v) for k, v in self.blame_lines.items())[::-1][:15])
         _, ax = plt.subplots()
         ax.stackplot(self.refs, self.blame_lines.values(),
             labels=self.blame_lines.keys(), alpha=0.8)
